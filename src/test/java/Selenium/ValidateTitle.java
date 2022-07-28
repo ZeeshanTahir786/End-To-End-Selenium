a200d33f088d938130fd6a6b1ebe44d327fe0a60
Hafiz ZeeShan <65600657+ZeeshanTahir786@users.noreply.github.com>
2022-07-28 14:27:51 +0500

    Update ValidateTitle.java

--- src/test/java/Selenium/ValidateTitle.java
+++ src/test/java/Selenium/ValidateTitle.java
@@ -1,14 +1,23 @@
 package Selenium;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import org.testng.AssertJUnit;
 import java.io.IOException;
 
 import pageObjects.LandingPage;
 import resources.Base;
 
 public class ValidateTitle extends Base {
+
+	@BeforeTest
+	public void initialize() throws IOException {
+		driver = initializeDriver();
+
+		driver.get("https://rahulshettyacademy.com");
+	}
 
 	@Test
 	public void basePageNavigation() throws IOException {
@@ -18,7 +27,11 @@ public class ValidateTitle extends Base {
 		driver.get("https://rahulshettyacademy.com");
 
 		LandingPage landingPage = new LandingPage(driver);
-		AssertJUnit.assertEquals(landingPage.getTitle().getText(), "Featured Courses");
+		Assert.assertEquals(landingPage.getTitle().getText(), "Featured Courses");
 	}
 
+	@AfterTest
+	public void tearDown() {
+		driver.close();
+	}
 }
